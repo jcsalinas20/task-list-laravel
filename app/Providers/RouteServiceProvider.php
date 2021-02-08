d2479a41893f1c1e6e1ae3362f299041a7a15451
jcsalinas20 <jcsalinasn20@gmail.com>
2021-02-08 18:47:19 +0100

    https

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -35,6 +35,7 @@ class RouteServiceProvider extends ServiceProvider
      */
     public function boot()
     {
+        resolve(\Illuminate\Routing\UrlGenerator::class)->forceScheme('https');
         $this->configureRateLimiting();
 
         $this->routes(function () {
